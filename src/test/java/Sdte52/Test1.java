dad02b81fcd1299284bc27d1b8c87cc65b97cb2b
DELL <DELL@DESKTOP-S5RPUEQ>
2023-11-28 11:08:44 +0530

    Demo ashwini project

---
+++ src/test/java/Sdte52/Test1.java
@@ -0,0 +1,18 @@
+package Sdte52;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test(groups="smoke")
+	public void ts3test()
+	{
+		System.out.println("--ts3---");
+	}
+	@Test(groups="regration")
+	public void ts2test()
+	{
+		System.out.println("--ts2---");
+	}
+	
+
+}
---
+++ src/test/java/Sdte52/TestScript34Test.java
@@ -0,0 +1,18 @@
+package Sdte52;
+
+import org.testng.annotations.Test;
+
+public class TestScript34Test {
+	@Test(groups = "smoke")
+	public void ts3Test()
+	{
+		System.out.println("---test TS3start---");
+	}
+	
+	@Test(groups="regration")
+	public void ts4Test()
+	{
+		System.out.println("----test TS4test----");
+	}
+
+}
