7dea77e8f8610456b89d53804ad44f3c6d218c52
DELL <DELL@DESKTOP-S5RPUEQ>
2023-11-28 11:33:42 +0530

    update

--- src/main/java/GenricUtiles/JavaUtiles.java
+++ src/main/java/GenricUtiles/JavaUtiles.java
@@ -1,5 +1,14 @@
 package GenricUtiles;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class JavaUtiles {
+	
+	public void ReadDtafrompropertiefile() throws FileNotFoundException
+	{
+		FileInputStream fis = new FileInputStream("./src/test/java/commons.properties");
+		
+	}
 
 }
