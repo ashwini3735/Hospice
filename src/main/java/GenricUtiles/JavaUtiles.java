fc0791440dd17f3e879d57f8e6a1e4a6a8a15fe3
DELL <DELL@DESKTOP-S5RPUEQ>
2023-11-28 12:18:54 +0530

    propetes obeject creatted

--- src/main/java/GenricUtiles/JavaUtiles.java
+++ src/main/java/GenricUtiles/JavaUtiles.java
@@ -2,13 +2,14 @@ package GenricUtiles;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Properties;
 
 public class JavaUtiles {
 	
 	public void ReadDtafrompropertiefile() throws FileNotFoundException
 	{
 		FileInputStream fis = new FileInputStream("./src/test/java/commons.properties");
-		
+		Properties pobj = new Properties();
 	}
 
 }
